368e9ef4f0ff6d79039553c09b8d08994e5aa123
KunalKp1997 <126089405+KunalKp1997@users.noreply.github.com>
2023-02-22 21:15:56 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,12 @@
+const express= require('express');
+const path=require('path');
+const app= express();
+
+app.set('view engine','ejs');
+app.use('/static',express.static(path.join(__dirname,'public')))
+const port= process.env.PORT||3000;
+
+app.get('/',(req,res)=>{
+    res.render('base',{title:"Login System"});
+})
+app.listen(port,()=>{console.log("Listening to the server on http://localhost:3000")});
